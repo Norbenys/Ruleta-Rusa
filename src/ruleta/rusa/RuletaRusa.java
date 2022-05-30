1042c1be9c34136fae732190defb49531b9c2e3d
Norbenys <102006255+Norbenys@users.noreply.github.com>
2022-05-29 22:18:51 -0400

    Comentario

--- src/ruleta/rusa/RuletaRusa.java
+++ src/ruleta/rusa/RuletaRusa.java
@@ -15,7 +15,9 @@ public class RuletaRusa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creación del proyecto 
+        
+        
     }
     
 }
